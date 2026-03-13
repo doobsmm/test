001819516d4f37792f79319fe76803276b8f48ce
doobsmm <doob.helpdesk@gmail.com>
2026-03-13 20:50:42 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+include "includes/config.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php echo $site_name; ?></title>
+<link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+
+<div class="container">
+
+<h1>Welcome to <?php echo $site_name; ?></h1>
+
+<p>Social Media Marketing Panel</p>
+
+<a href="login.php">Login</a>
+<a href="register.php">Register</a>
+
+</div>
+
+</body>
+</html>
